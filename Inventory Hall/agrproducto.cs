5a28ed71121d45ba8eab71c738c0a0a7c83bb0db
Daniel04r <Jondanieljon26@hotmail.com>
2023-12-05 04:07:47 -0400

    agregue el boton y le di funcionabilidad a bonton NUEVO

--- Inventory Hall/agrproducto.cs
+++ Inventory Hall/agrproducto.cs
@@ -13,12 +13,42 @@ namespace Inventory_Hall
 
     public partial class agrproducto : Form
     {
+        public agrproducto()
+        {
+            InitializeComponent();
+
+            ApartadoCategoria();
 
 
 
+        }
+
+        private void ApartadoCategoria()
+        {
+        
+            String consulta = "SELECT id, tipo FROM categoria";
+
+
+        }
+        
+
+        
         private void agrproducto_Load(object sender, EventArgs e)
         {
+            txtnombre.Enabled = false;
+            txtcategoria.Enabled = false;
+            txtdescripcion.Enabled = false;
+            txtstock.Enabled = false;
+            txtidsuplidor.Enabled = false;
+            txtseccion.Enabled = false;
 
+            txtnombre.BackColor = Color.Gray;
+            txtcategoria.BackColor = Color.Gray;
+            txtdescripcion.BackColor = Color.Gray;
+            txtstock.BackColor = Color.Gray; 
+            txtidsuplidor.BackColor = Color.Gray;
+            txtseccion.BackColor = Color.Gray;
+            
         }
 
         private void txtcategoria_SelectedIndexChanged(object sender, EventArgs e)
@@ -33,6 +63,19 @@ namespace Inventory_Hall
 
         private void btnnuevo_Click(object sender, EventArgs e)
         {
+            txtnombre.Enabled = true;
+            txtcategoria.Enabled = true;
+            txtdescripcion.Enabled = true;
+            txtstock.Enabled = true;
+            txtidsuplidor.Enabled = true;
+            txtseccion.Enabled = true;
+
+            txtnombre.BackColor = Color.White;
+            txtcategoria.BackColor = Color.White;
+            txtdescripcion.BackColor = Color.White;
+            txtstock.BackColor = Color.White;
+            txtidsuplidor.BackColor = Color.White;
+            txtseccion.BackColor = Color.White;
 
         }
 
